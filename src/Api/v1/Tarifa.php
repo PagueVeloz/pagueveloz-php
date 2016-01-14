19715bc72baf0d81862a95c6928a0640052a3055
Cristian B.San <cristian.deveng@gmail.com>
2016-01-14 14:47:42 -0200

    Consulta de tarifa por tipo de operação

--- src/Api/v1/Tarifa.php
+++ src/Api/v1/Tarifa.php
@@ -18,6 +18,18 @@ use \PagueVeloz\Api\Common\Auth;
 
 class Tarifa extends ServiceProvider implements InterfaceApi
 {
+
+	const TIPO_NAODEFINIDO           = 0;
+	const TIPO_BOLETOEMISSAO         = 1;
+	const TIPO_BOLETOLIQUIDACAO      = 2;
+	const TIPO_SAQUE                 = 3;
+	const TIPO_NAOTARIFAR            = 4;
+	const TIPO_TRANSFERENCIAEFETUADA = 5;
+	const TIPO_TRANSFERENCIARECEBIDA = 6;
+	const TIPO_DEPOSITORECEBIDO      = 7;
+	const TIPO_PAGAMENTODECONTA      = 8;
+	const TIPO_BOLETOBAIXAMANUAL     = 9;
+
 	public function __construct()
 	{
 
@@ -41,6 +53,16 @@ class Tarifa extends ServiceProvider implements InterfaceApi
 
 		return $this->NoContent();
 
+	}
+
+	public function CalcularByTipo($tipo, $valor)
+	{
+		$this->url = sprintf('%s/Calcular?tipo=%s&valor=%01.2f',$this->url,$tipo,$valor);
+
+		$this->method = 'GET';
+		$this->Authorization();
+
+		return $this->init();
 	}
 
 	public function Post()
