0a54bd3987b21807261d857de51c3269bb0d539a
Jonathan A. Schweder <jonathanschweder@gmail.com>
2016-06-29 11:50:21 -0300

    adicionado testes para TitularContaBancariaDTO

---
+++ tests/Api/Common/Dto/TitularContaBancariaDTOTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace PagueVeloz\Api\Common\Dto;
+
+use TestCase;
+
+class TitularContaBancariaDTOTest extends TestCase
+{
+    protected $dto;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->dto = new TitularContaBancariaDTO();
+    }
+
+    public function testGetNotEmpty()
+    {
+        $this->assertEquals($this->dto->getNotEmpty(), []);
+    }
+
+    public function testSetNome()
+    {
+        $nome = $this->faker->name;
+
+        $this->dto->setNome($nome);
+
+        $this->assertAttributeEquals($nome, 'Nome', $this->dto);
+
+        return $nome;
+    }
+
+    /**
+     * @depends testSetNome
+     */
+    public function testGetNome($nome)
+    {
+        $this->dto->setNome($nome);
+
+        $this->assertEquals($nome, $this->dto->getNome());
+    }
+
+    public function testSetDocumento()
+    {
+        $cpf = $this->faker->cpf;
+
+        $this->dto->setDocumento($cpf);
+
+        $this->assertAttributeEquals($cpf, 'Documento', $this->dto);
+
+        return $cpf;
+    }
+
+    /**
+     * @depends testSetDocumento
+     */
+    public function testGetDocumento($cpf)
+    {
+        $this->dto->setDocumento($cpf);
+
+        $this->assertEquals($cpf, $this->dto->getDocumento());
+    }
+
+    public function testSetTipoPessoa()
+    {
+        $tipo = $this->faker->randomElement([0,1,2]);
+
+        $this->dto->setTipoPessoa($tipo);
+
+        $this->assertAttributeEquals($tipo, 'TipoPessoa', $this->dto);
+
+        return $tipo;
+    }
+
+    /**
+     * @depends testSetTipoPessoa
+     */
+    public function testGetTipoPessoa($tipo)
+    {
+        $this->dto->setTipoPessoa($tipo);
+
+        $this->assertEquals($tipo, $this->dto->getTipoPessoa());
+    }
+}
